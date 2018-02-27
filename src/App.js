62dada4a54dda11300ef134f18e757d00ef2c3d6
Ehsan <ehsankhfr@gmail.com>
2018-02-27 13:55:46 +0000

    Router Links and subviews

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './Home';
+import Roster from './Roster';
+
+import { Link } from 'react-router-dom'
+
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
@@ -9,10 +19,22 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <nav>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/roster'>Roster</Link></li>
+            </ul>
+          </nav>
         </header>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          {/* both /roster and /roster/:number begin with /roster */}
+          <Route path='/roster' component={Roster}/>
+        </Switch>
       </div>
     );
   }
